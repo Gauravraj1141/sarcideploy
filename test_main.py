a0b2c02c5225ad46540ecda65e35bb6d3655a124
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-06-10 14:34:54 +0530

    adding some func

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from appconfig.inventorymanagement.inventorymanagementapp.main import hello_world
+from inventorymanagementapp.main import hello_world
 
 def test_hello_world():
     result = hello_world()
