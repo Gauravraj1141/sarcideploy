60f16030ae0b9f4f07236addd1bc838ac6e01e1f
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-06-10 13:46:30 +0530

    adding some func

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from sarcideploy.appconfig.main import hello_world
+from appconfig.inventorymanagement.inventorymanagementapp.main import hello_world
 
 def test_hello_world():
     result = hello_world()
